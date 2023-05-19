76f2a3b55d30bb0f6a5ae33adfe46f2b019f2ace
Shushant Rishav <54536673+Rishu20@users.noreply.github.com>
2023-05-19 09:30:36 +0530

    Update Wordcloud.py

--- Wordcloud.py
+++ Wordcloud.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
-import wordcloud
-file1 = "/home/rishu/Programming/Programmings/zz_images_files/file.txt"
-file2 = "/home/rishu/Programming/Programmings/zz_images_files/temp.txt"
+from wordcloud import WordCloud as wc
+import docx
+
 punctuations = ['!', '"', '#', "\n" , '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=','>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~', "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 uninteresting_words = ["the", "mr", "mrs", "for", "us", "a", "so", "to", "if", "is", "not", "on", "it", "of", "and", "or", "an", "as", "in", "i", "me", "my",
                        "we", "our", "ours", "you", "your", "yours", "he", "she", "him", "his", "her", "hers", "its", "they", "them",
@@ -9,36 +9,51 @@ uninteresting_words = ["the", "mr", "mrs", "for", "us", "a", "so", "to", "if", "
                         "have", "has", "had", "do", "does", "did", "but", "at", "by", "with", "from", "here", "when", "where", "how",
                        "all", "any", "both", "each", "few", "more", "some", "such", "no", "nor", "too", "very", "can", "will", "just", "than"]
 
-new_content = ""
-with open(file1, "r+") as fin, open(file2,"w+") as fout:  
+def clean_text(text):
+  cleaned_text = ""
+  for line in text:
+      for chr in punctuations:
+          line = line.replace(chr, "")
+      cleaned_text += line
+  return cleaned_text
 
-    for line in fin:
-        for chr in punctuations:
-            line = line.replace(chr, "")
-        fout.write(line)
+def count_frequency(cleaned_text):
+  new_content = ""
+  for chr in cleaned_text:
+      new_content += chr
+  temp_file = new_content.split(" ")
+  frequencies = {}
+  for word in temp_file:
+      if word.lower() not in uninteresting_words:
+          if word.lower() in frequencies:
+              frequencies[word.lower()] +=1
+          if word.lower() not in frequencies:
+              frequencies[word.lower()] = 1
+  freq = {k: v for k, v in sorted(frequencies.items(), key=lambda item: item[1])}
+  return freq
 
-file_contents = open(file2, "r", encoding="utf-8")
+def generate_cloud(text):
+  c_text = clean_text(text)
+  frequencies = count_frequency(c_text)
+  cloud = wc(background_color="black",colormap='Blues',width=1200,height=720)
+  cloud.generate_from_frequencies(frequencies)
 
-new_content = ""
-for chr in file_contents:
-    new_content += chr
-temp_file = new_content.split(" ")
-frequencies = {}
-for word in temp_file:
-    if word.lower() not in uninteresting_words:
-        if word.lower() in frequencies:
-            frequencies[word.lower()] +=1
-        if word.lower() not in frequencies:
-            frequencies[word.lower()] = 1
+  # Display your wordcloud image
+  cloud.to_file("media/word.png")
 
-#with open(file3, "w") as file3:
-#    file3.write(str(frequencies))
 
-cloud = wordcloud.WordCloud(background_color="white",width=1200,height=720)
-cloud.generate_from_frequencies(frequencies)
+def process_text(file_text):
+  with open(file_text, "r+",encoding='utf-8') as fin:
+    text = fin.readlines()
+  generate_cloud(text)
+  token = 'Done'
+  return token
 
-# Display your wordcloud image
-cloud.to_file("/home/rishu/Programming/Programmings/zz_images_files/word.png")
-plt.imshow(cloud, interpolation='bilinear')
-plt.axis("off")
-plt.show()
+def process_docx(file_docx):
+  text = ''
+  doc = docx.Document(file_docx)
+  for paragraph in doc.paragraphs:
+    text += (paragraph.text)
+  generate_cloud(text)
+  token = 'Done'
+  return token
